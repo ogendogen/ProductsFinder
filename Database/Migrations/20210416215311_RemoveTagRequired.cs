9e8f4c0011b12333ee0abc0a6e65372ccbbaf24e
ogendogen <kosmos500@interia.pl>
2021-04-16 23:54:20 +0200

    Removed tag required migration

---
+++ Database/Migrations/20210416215311_RemoveTagRequired.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class RemoveTagRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Tag",
+                table: "Products",
+                type: "TEXT",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 128);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Tag",
+                table: "Products",
+                type: "TEXT",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+    }
+}
